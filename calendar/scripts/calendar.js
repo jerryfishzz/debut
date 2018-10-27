acfa995c324ca4659fe5a8f7938199f49c96ee5c
jerryfishzz <jerryfish@163.com>
2018-10-27 16:40:35 +1300

    Modified login/logout and add issue on calendar
    1. changed logout path to the unique entrance
    2. modified the add issue bug

--- calendar/scripts/calendar.js
+++ calendar/scripts/calendar.js
@@ -201,7 +201,7 @@ $(document).ready(function(){
 			});
 		} else {  //用户已退出或超时
 			alert("用户已退出或用户已被删除。");
-			document.location.href = "login.php";
+			document.location.href = "../login.php";
 		}
 	};
 	
@@ -266,7 +266,7 @@ $(document).ready(function(){
 					}
 				} else {
 					alert("用户已退出或用户已被删除。。");
-					document.location.href = "login.php";
+					document.location.href = "../login.php";
 				}
 			}
 		});
@@ -360,7 +360,7 @@ $(document).ready(function(){
 	
 	//显示邻近三个月的事件。不检查用户权限。
 	function eventsOnCalendar(selectedDate, id) {
-		
+		console.log("eventsOnCalendar");
 		var selectedDtArray = selectedDate.split("-");
 		var selectedYear = selectedDtArray[0];
 		var selectedMonth = selectedDtArray[1];
@@ -419,6 +419,7 @@ $(document).ready(function(){
 			encode : true,
 
 			success : function(events) {
+				console.log("Begin add");
 				var adjacentThreeMonthsItemsMyNumArrayInDb = [];  //数据库中邻近三个月的myNum值数组
 				
 				//返回值events为邻近三个月的记录，需要有记录内容才开始进行添加。
@@ -862,7 +863,6 @@ $(document).ready(function(){
 							success : function(addEvent) {
 								if(addEvent.action) {
 									$("#dateSelect").val(startDate);
-
 									$("#dateSelect").bind('change', function() {
 										var selectedDate = $("#dateSelect").val();
 										var dtArray = selectedDate.split("-");
@@ -877,27 +877,9 @@ $(document).ready(function(){
 
 										//当日期选择栏发生变化时促发每月事件显示
 										eventsOnCalendar(selectedDate, id);
-									//}).change();
-									});
+									}).change();
 								} else {
 									alert(addEvent.msg);
-									//document.location.href = "calendar.php?id="+id;
-									
-									/*
-									$("#dateSelect").val(startDate);
-
-									$("#dateSelect").bind('change', function() {
-										var selectedDate = $("#dateSelect").val();
-										var dtArray = selectedDate.split("-");
-										var year = dtArray[0];
-										var month = dtArray[1];
-										month = month.replace(/^[0]+/g,"")		
-										var day = dtArray[2];
-										jfcalplugin.showMonth("#mycal",year,parseInt(month-1).toString());
-									
-										eventsOnCalendar(selectedDate, id);
-									});
-									*/
 								}
 							}
 						});
